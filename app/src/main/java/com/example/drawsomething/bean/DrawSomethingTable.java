f0a3cc0528b6fcf1b6b2207f77802014deec50eb
Administrator <1316172397@qq.com>
2019-12-10 16:24:29 +0800

    secont commit

---
+++ app/src/main/java/com/example/drawsomething/bean/DrawSomethingTable.java
@@ -0,0 +1,67 @@
+package com.example.drawsomething.bean;
+
+public class DrawSomethingTable {
+    private int id;
+    private String user_1;
+    private String user_2;
+    private String user_3;
+    private String user_4;
+    private String last_check;
+    private int game_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUser_1() {
+        return user_1;
+    }
+
+    public void setUser_1(String user_1) {
+        this.user_1 = user_1;
+    }
+
+    public String getUser_2() {
+        return user_2;
+    }
+
+    public void setUser_2(String user_2) {
+        this.user_2 = user_2;
+    }
+
+    public String getUser_3() {
+        return user_3;
+    }
+
+    public void setUser_3(String user_3) {
+        this.user_3 = user_3;
+    }
+
+    public String getUser_4() {
+        return user_4;
+    }
+
+    public void setUser_4(String user_4) {
+        this.user_4 = user_4;
+    }
+
+    public String getLast_check() {
+        return last_check;
+    }
+
+    public void setLast_check(String last_check) {
+        this.last_check = last_check;
+    }
+
+    public int getGame_id() {
+        return game_id;
+    }
+
+    public void setGame_id(int game_id) {
+        this.game_id = game_id;
+    }
+}
